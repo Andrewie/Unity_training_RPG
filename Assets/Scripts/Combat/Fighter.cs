7ed665c9fb10a7a70a09834c85e717768414ce5f
Andrewie <andreassundstr03m@gmail.com>
2019-08-24 10:02:36 +0200

    0.001

---
+++ Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using RPG.Movement;
+
+
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour
+    {
+        Transform target;
+        [SerializeField] float weaponRange = 2f;
+        private void Update()
+        {
+            if (target != null)
+            {
+                GetComponent<Mover>().MoveTo(target.position);
+            }
+
+
+
+        }
+        public void Attack(CombatTarget combatTarget)
+        {
+            target = combatTarget.transform;
+        }
+    }
+}
